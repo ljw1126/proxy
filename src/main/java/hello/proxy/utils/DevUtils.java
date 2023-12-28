5e10f745eb2a43c1213b5043f68731da4221aee3
leejinwoo <leejinwoo1126@gmail.com>
2023-12-28 11:56:46 +0900

    feat: DevUtils에 sleep 정적 메소드 추가

---
+++ src/main/java/hello/proxy/utils/DevUtils.java
@@ -0,0 +1,11 @@
+package hello.proxy.utils;
+
+public class DevUtils {
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
