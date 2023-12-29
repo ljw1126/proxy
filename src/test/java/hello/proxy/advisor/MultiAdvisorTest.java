e66b43f52132535e5cd7624a55c743e1aaf69a69
leejinwoo <leejinwoo1126@gmail.com>
2023-12-29 17:50:28 +0900

    test: 예제4 하나의 프록시에 여러 Advisor 적용

---
+++ src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,78 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+@Slf4j
+public class MultiAdvisorTest {
+
+    @DisplayName("여러 프록시에 advisor 적용")
+    @Test
+    void multiAdvisorTest1() {
+        // client -> proxy2(advisor2) -> proxy1 (advisor1) -> target
+
+        // 프록시 1 생성
+        ServiceInterface target = new ServiceImpl();
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
+
+        // 프록시 2 생성
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+        ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
+        proxyFactory2.addAdvisor(advisor2);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
+
+        // 실행
+        proxy2.save();
+    }
+
+    @DisplayName("하나의 프록시, 여러 advisor를 적용")
+    @Test
+    void multiAdvisorTest2() {
+        // client -> proxy2(advisor2) -> proxy1 (advisor1) -> target
+
+        // 프록시 1 생성
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvisor(advisor2);
+        proxyFactory.addAdvisor(advisor1);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        // 실행
+        proxy.save();
+    }
+
+    @Slf4j
+    static class Advice1 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice 1 호출");
+            return invocation.proceed();
+        }
+    }
+
+    @Slf4j
+    static class Advice2 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice 2 호출");
+            return invocation.proceed();
+        }
+    }
+}
