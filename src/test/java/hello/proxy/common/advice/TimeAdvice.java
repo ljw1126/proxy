d7debaa16589a86c39e333fff57cc41420d82785
leejinwoo <leejinwoo1126@gmail.com>
2023-12-29 17:22:50 +0900

    test: 프록시 팩토리 예제1

---
+++ src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,22 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor { // 패키지 주의*
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = invocation.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = startTime - endTime;
+        log.info("result time 종료 = {}ms", resultTime);
+
+        return result;
+    }
+}
